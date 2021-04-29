5760d0829f4f4182a1761bbaa19cdd58267a8328
wellsky <wellsky@mail.ru>
2021-04-29 14:44:15 +0300

    MainActivity start changes

--- app/src/main/java/ru/netology/MainActivity.kt
+++ app/src/main/java/ru/netology/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.netology
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ImageButton>(R.id.likeIcon).setOnClickListener {
+            println("Like it")
+        }
     }
 }
