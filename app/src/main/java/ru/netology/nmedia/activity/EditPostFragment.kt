57dadffee9969ccdde9d7ec4ec93753b71712e13
wellsky <wellsky@mail.ru>
2021-09-08 21:42:03 +0300

    ImagePicker added

--- app/src/main/java/ru/netology/nmedia/activity/EditPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/EditPostFragment.kt
@@ -1,10 +1,17 @@
 package ru.netology.nmedia.activity
 
+import android.app.Activity
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toFile
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.github.dhaval2404.imagepicker.ImagePicker
+import com.github.dhaval2404.imagepicker.constant.ImageProvider
+import com.google.android.material.snackbar.Snackbar
 import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.FragmentEditPostBinding
 import ru.netology.nmedia.util.AndroidUtils
@@ -52,6 +59,64 @@ class EditPostFragment : Fragment() {
         //arguments?.textArg?.let(binding.editorText::setText)
         binding.editorText.setText(viewModel.edited.value?.content ?: "")
 
+        val pickPhotoLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                when (it.resultCode) {
+                    ImagePicker.RESULT_ERROR -> {
+                        Snackbar.make(
+                            binding.root,
+                            ImagePicker.getError(it.data),
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                    Activity.RESULT_OK -> {
+                        val uri: Uri? = it.data?.data
+                        viewModel.changePhoto(uri, uri?.toFile())
+                    }
+                }
+            }
+
+        binding.pickPhoto.setOnClickListener {
+            ImagePicker.with(this)
+                .crop()
+                .compress(2048)
+                .provider(ImageProvider.GALLERY)
+                .galleryMimeTypes(
+                    arrayOf(
+                        "image/png",
+                        "image/jpeg",
+                    )
+                )
+                .createIntent(pickPhotoLauncher::launch)
+        }
+
+        binding.takePhoto.setOnClickListener {
+            ImagePicker.with(this)
+                .crop()
+                .compress(2048)
+                .provider(ImageProvider.CAMERA)
+                .createIntent(pickPhotoLauncher::launch)
+        }
+
+        binding.removePhoto.setOnClickListener {
+            viewModel.changePhoto(null, null)
+        }
+
+        viewModel.postCreated.observe(viewLifecycleOwner) {
+            findNavController().navigateUp()
+        }
+
+        viewModel.photo.observe(viewLifecycleOwner) {
+            if (it.uri == null) {
+                binding.photoContainer.visibility = View.GONE
+                return@observe
+            }
+
+            binding.photoContainer.visibility = View.VISIBLE
+            binding.photo.setImageURI(it.uri)
+        }
+
+        /*
         binding.saveButton.setOnClickListener {
             viewModel.changeContent(binding.editorText.text.toString())
             viewModel.save()
@@ -64,6 +129,7 @@ class EditPostFragment : Fragment() {
             viewModel.loadPosts()
             findNavController().navigateUp()
         }
+        */
 
         return binding.root
     }
--- app/src/main/java/ru/netology/nmedia/db/AppDb.kt
+++ app/src/main/java/ru/netology/nmedia/db/AppDb.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import ru.netology.nmedia.dao.Converters
 import ru.netology.nmedia.dao.PostDao
 import ru.netology.nmedia.entity.PostEntity
 
-@Database(entities = [PostEntity::class], version = 18, exportSchema = false)
+@Database(entities = [PostEntity::class], version = 19, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class AppDb : RoomDatabase() {
     abstract fun postDao(): PostDao
 
